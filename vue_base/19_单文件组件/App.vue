1c86976b704f200d119f0b443e986aeb99119582
niecheng <614154335@qq.com>
2022-05-28 19:52:00 +0800

    first commit

---
+++ vue_base/19_单文件组件/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <School></School>
+    </div>
+  
+</template>
+
+<script>
+    // 引入组件
+    import School from './School'
+    export default {
+        name:'App',
+        data() {
+            return {
+                
+            }
+        },
+        components:{
+            School
+        }
+
+    }
+</script>
+
+<style>
+
+</style>
---
+++ vue_base/19_单文件组件/School.vue
@@ -0,0 +1,17 @@
+<template>
+    <!-- 组件的结构 -->
+    <div>
+        <h1>{{name}}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'School',
+    data() {
+        return {
+            name:"尚硅谷"
+        }
+    },
+}
+</script>
